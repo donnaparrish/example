536d6d8ddb9115c1329b746f9a2c5f8f1da12c8d
donnaparrish <dparrish@sbeco.net>
2016-09-20 11:12:54 -0400

    first commit

---
+++ example/Client.js
@@ -0,0 +1,18 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       20 Sep 2016     Donna Parrish
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType 
+ * 
+ * @param {String} type Access mode: create, copy, edit
+ * @returns {Void}
+ */
+function clientPageInit(type){
+   
+}
